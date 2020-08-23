523d6f39d36822fc35d9d8927a770e1427c1d05c
Bijo <bijo@itu.dk>
2020-08-23 23:14:40 +0200

    Added test for exercise input file

--- PyramidChallenge/PyramidChallenge/PyramidChallenge.Test/ReaderTest.cs
+++ PyramidChallenge/PyramidChallenge/PyramidChallenge.Test/ReaderTest.cs
@@ -7,7 +7,7 @@ namespace PyramidChallenge.Test
     public class ReaderTest
     {
         [TestMethod]
-        public void GivenInputTriangle_OnGetTriangle_ValidateInputIsFiltered()
+        public void GivenSmallTestFile_OnReadInput_ValidateNodesInserted()
         {
 
             var reader = new Reader();
@@ -19,6 +19,19 @@ namespace PyramidChallenge.Test
             AssertNodeValue(tree.Left.Right, 5, 2);
 
             tree.Left.Left.Left.Left.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void GivenTestFile_OnReadInput_ValidateNodesInserted()
+        {
+
+            var reader = new Reader();
+            var tree = reader.ReadInput(@"./TestPyramid.txt");
+
+            AssertNodeValue(tree, 192, 124);
+            AssertNodeValue(tree.Left, 117, 269);
+            AssertNodeValue(tree.Left.Left, 218, 836);
+            AssertNodeValue(tree.Left.Right, 836, 347);
         }
 
         public void AssertNodeValue(Node node, int left, int right)
