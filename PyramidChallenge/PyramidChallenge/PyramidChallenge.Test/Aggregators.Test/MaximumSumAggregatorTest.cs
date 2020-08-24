dc1d8a1f6985ddfd4e5e8005a84b62f9b1e87d37
Bijo <bijo@itu.dk>
2020-08-25 00:21:07 +0200

    Implement method for finding max sum and add test

---
+++ PyramidChallenge/PyramidChallenge/PyramidChallenge.Test/Aggregators.Test/MaximumSumAggregatorTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PyramidChallenge.Test
+{
+    [TestClass]
+    public class MaximumSumAggregatorTest
+    {
+        [TestMethod]
+        public void GivenExerciseSample_OnFindMaxValue_ValidateReturnValueIsMax()
+        {
+            // Arrange
+            var reader = new NodeReader();
+            var rootNode = reader.ReadInput(@"./Data/ExerciseExample.txt");
+            var aggregator = new MaximumSumAggregator();
+
+            //Act
+            var res = aggregator.FindMaxValue(rootNode);
+
+            //Assert
+            Assert.AreEqual(16, res);
+        }
+
+        [TestMethod]
+        public void GivenMadeUpSample_OnFindMaxValue_ValidateReturnValueIsMax()
+        {
+            // Arrange
+            var reader = new NodeReader();
+            var rootNode = reader.ReadInput(@"./Data/MadeUpExample.txt");
+            var aggregator = new MaximumSumAggregator();
+
+            //Act
+            var res = aggregator.FindMaxValue(rootNode);
+
+            //Assert
+            Assert.AreEqual(18, res);
+        }
+
+        [TestMethod]
+        public void GivenPyramidChallenge_OnFindMaxValue_ValidateReturnValueIsMax()
+        {
+            // Arrange
+            var reader = new NodeReader();
+            var rootNode = reader.ReadInput(@"./Data/PyramidChallenge.txt");
+            var aggregator = new MaximumSumAggregator();
+
+            //Act
+            var res = aggregator.FindMaxValue(rootNode);
+
+            //Assert
+            Assert.AreEqual(8186, res);
+        }
+    }
+}
---
+++ PyramidChallenge/PyramidChallenge/PyramidChallenge/Aggregators/MaximumSumAggregator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PyramidChallenge
+{
+    /// <summary>
+    /// This class implements an algorithm finding the maximum sum of a given pyramid/tree. 
+    /// It is implement such that it reads from top to bottom, changes between even and odd numbers
+    /// and assumes that is at least one valid path.
+    /// </summary>
+    public class MaximumSumAggregator
+    {
+        public long FindMaxValue(Node rootNode)
+        {
+            rootNode.MaxValue = rootNode.Value + FindMaxValue(rootNode, rootNode.Value % 2 == 0);
+            return rootNode.MaxValue;
+        }
+
+        private long FindMaxValue(Node node, bool previousNodeWasEven)
+        {
+            if (node.Left == null)
+            {
+                return default;
+            }
+            else
+            {
+                var left = node.Left;
+                var right = node.Right;
+
+                if (left.MaxValue == default && IsOppositeOfPrev(left.Value, previousNodeWasEven))
+                {
+                    left.MaxValue = left.Value + FindMaxValue(left, !previousNodeWasEven);
+                }
+
+                if (right.MaxValue == default && IsOppositeOfPrev(right.Value, previousNodeWasEven))
+                {
+                    right.MaxValue = right.Value + FindMaxValue(right, !previousNodeWasEven);
+                }
+
+                return Math.Max(right.MaxValue, left.MaxValue);
+            }
+        }
+
+        private bool IsOppositeOfPrev(int value, bool prev)
+        {
+            return (value % 2 == 0) != prev;
+        }
+    }
+}
